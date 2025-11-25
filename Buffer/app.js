41ea3bc350dcd55c072c4ca536c7cdf58ae07190
Sai Kiran <saikiran12040@gmail.com>
2025-11-25 16:08:07 +0530

    added

---
+++ Buffer/app.js
@@ -0,0 +1,28 @@
+const {Buffer}=require('buffer');
+
+const memoryContainer=Buffer.alloc(4)
+
+memoryContainer[0]=0xf4;
+memoryContainer[1]=0x34;
+memoryContainer[2]=0x00;
+memoryContainer[3]=0xff;
+
+console.log(memoryContainer)
+console.log(memoryContainer[0])
+console.log(memoryContainer[1])
+console.log(memoryContainer[2])
+console.log(memoryContainer[3])
+
+console.log(memortContainer.toString('hex'))
+
+// const buff=Buffer.from([0x48,0x69,0x21])
+// console.log(buff.toString('utf-8'))
+
+const buff=Buffer.from('486921','hex')
+console.log(buff.toString('utf-8'))
+
+const strBuff=Buffer.from('Hi!','utf-8')
+console.log(strBuff)
+
+
+
